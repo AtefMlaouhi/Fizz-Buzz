726c205398f5d0a546006cb837a3757aa7996c9b
Atef Mlaouhi <atef.mlaouhi@outlook.com>
2019-01-23 10:27:43 +0100

    implement case : Any number does not meet rules must return oops.

--- src/shared/services/fizz-buzz/fizz-buzz.service.spec.ts
+++ src/shared/services/fizz-buzz/fizz-buzz.service.spec.ts
@@ -13,10 +13,12 @@ describe('FizzBuzzService', () => {
   it('getFizzBuzzFromNumber(0) should return `oops`', () => {
     const service: FizzBuzzService = TestBed.get(FizzBuzzService);
 
-    service.getFizzBuzzFromNumber('0').subscribe((result: string[]) => {
-      expect(result.length).toEqual(1);
-      expect(result[0]).toEqual('oops');
-    });
+    service
+      .getFizzBuzzFromNumber('0')
+      .subscribe((result: string[]) => {
+        expect(result.length).toEqual(1);
+        expect(result[0]).toEqual('oops');
+      })
+      .unsubscribe();
   });
-
 });
--- src/shared/services/fizz-buzz/fizz-buzz.service.ts
+++ src/shared/services/fizz-buzz/fizz-buzz.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FizzBuzzService {
+  private readonly three = '3';
+  private readonly five = '5';
+  private readonly Fizz = 'Fizz';
+  private readonly Buzz = 'Buzz';
+  private readonly Oops = 'oops';
+
   constructor() {}
 
-  getFizzBuzzFromNumber(arg0: string): any {
-    throw new Error('Method not implemented.');
+  getFizzBuzzFromNumber(value: string): Observable<string[]> {
+    const retValue = new Array<string>();
+    if (!value.includes(this.three) || !value.includes(this.five)) {
+      retValue.push(this.Oops);
+    }
+
+    return of<string[]>(retValue);
   }
 }
